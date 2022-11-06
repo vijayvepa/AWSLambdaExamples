9b8d7a821d2e1ffb4163d452e88d0b7310a98fca
Vijay <vijay.k.vepakomma@gmail.com>
2022-11-06 16:08:43 -0500

    Visual Studio Lambda Hello World!

---
+++ LambdaHelloWorld/LambdaHelloWorld/app.js
@@ -0,0 +1,25 @@
+﻿console.log('Loading Function');
+
+exports.handler = function (event, context) {
+
+    if (event != null) {
+        console.log('event = ' + JSON.stringify(event));
+    }
+    else {
+        console.log('No event object');
+
+    }
+
+    var name = '';
+    if ('name' in event) {
+        name = event['name']
+    } else {
+        name = 'World'
+    }
+
+    var greeting = 'Hello ' + name + "!";
+    console.log(greeting);
+
+    context.done(null, greeting);  // SUCCESS with message
+    return greeting;
+};
